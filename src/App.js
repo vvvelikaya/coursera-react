98c24c84da667392327be42382ca4d7460cc0a7d
Veronica Velikaya <nik.velikay@gmail.com>
2021-03-28 23:52:17 +0300

    components prt2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap'
+import './App.css'
 import Menu from './components/Menu'
+import { DISHES } from './shared/dishes'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +23,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
