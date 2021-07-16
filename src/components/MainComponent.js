aee48d2aeb772ef8cb261bd921b81b43ab1328ef
Veronica Velikaya <nik.velikay@gmail.com>
2021-07-16 20:35:28 +0300

    single page application prt2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -28,8 +28,15 @@ class Main extends React.Component {
         const HomePage = () => {
             return (
                 <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            )
+        }
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             )
         }
 
@@ -39,6 +46,7 @@ class Main extends React.Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
